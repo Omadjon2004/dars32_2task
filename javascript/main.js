54dbe916e483140fe76674b46077fa2d26066a00
Omadjon2004 <122809872+Omadjon2004@users.noreply.github.com>
2023-03-09 22:49:50 +0500

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10,7778,-156];
+
+
+const multiplyNum=function(num){
+    
+    return num*2
+   }
+   
+   for (let i = 0; i < numbers.length; i++) {
+       console.log(multiplyNum(numbers[i]));
+   }
